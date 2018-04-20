12b6969eb8e8614147a905d178c6ff96f806ccb4
tstock96 <tstockham96@gmail.com>
2018-04-20 19:50:24 -0400

    Put this all in comments so we can run graphics

--- testing.cpp
+++ testing.cpp
@@ -1,145 +1,145 @@
+////
+//// Created by Thomas Stockham on 4/18/18.
+////
 //
-// Created by Thomas Stockham on 4/18/18.
+//#include <iostream>
+//#include "shapes.h"
+//#include "rocket.h"
+//#include "graphics.h"
+//#include "fueltank.h"
 //
-
-#include <iostream>
-#include "shapes.h"
-#include "rocket.h"
-#include "graphics.h"
-#include "fueltank.h"
-
-using namespace std;
-
-int main() {
-    /********** TESTING FOR RECT CLASS ***********/
-
-    //default constructor testing
-    Rect r;
-    if (r.getCenter().x != 0 && r.getCenter().y != 0) {
-        cout << "FAILED rectangle default constructor point test" << endl;
-    }
-    if (r.getArea() != 0) {
-        cout << "FAILED Rectangle default constructor area test" << endl;
-    }
-    if (r.getFill().red != 0 && r.getFill().blue != 0 && r.getFill().green != 0) {
-        cout << "FAILED Rectanble default constructor fill test" << endl;
-    }
-    if (r.getPerimeter() != 0) {
-        cout << "FAILED Rectangle default constructor perimeter test" << endl;
-    }
-    //non-default testing 1
-    Rect r1(0.0, 0.0);
-    r1.setDimensions(2, 2);
-    if (r1.getWidth() != 2 && r1.getHeight() != 2) {
-        cout << "FAILED rect non-default constructor getwidth/setdim test" << endl;
-    }
-
-    if (r1.getPerimeter() != 0) {
-        cout << "FAILED shape non-default constructor perimeter test" << endl;
-    }
-
-    // Getters and setters testing
-    r.setPoint({1, 1});
-    if (r.getCenter().x != 1 && r.getCenter().y != 1) {
-        cout << "FAILED shape getter and setter point 1-val test" << endl;
-    }
-    r.setPoint(2, 2);
-    if (r.getCenter().x != 2 && r.getCenter().y != 2) {
-        cout << "FAILED shape getter and setter point 2-val test" << endl;
-    }
-    r.move(2, 2);
-    if (r.getCenter().x != 4 && r.getCenter().y != 4) {
-        cout << "FAILED shape getter and setter move test" << endl;
-    }
-    if (r.getPerimeter() != 8) {
-        cout << "FAILED rect perimiter test" << endl;
-    }
-    if (r.getArea() != 4) {
-        cout << "FAILED shape area test" << endl;
-    }
-
-    /*************** TESTING FOR CIRCLE CLASS *************/
-
-    Circle c;
-    if (c.getCenter().x != 0 && c.getCenter().y != 0) {
-        cout << "FAILED circle default constructor point test" << endl;
-    }
-    if (c.getArea() != 0) {
-        cout << "FAILED circle default constructor area test" << endl;
-    }
-    if (c.getFill().red != 0 && c.getFill().blue != 0 && c.getFill().green != 0) {
-        cout << "FAILED Circle default constructor fill test" << endl;
-    }
-    if (c.getPerimeter() != 0) {
-        cout << "FAILED Circle default constructor perimeter test" << endl;
-    }
-    //non-default
-    Circle c1(10);
-    if (c1.getCenter().x != 0 && c1.getCenter().y != 0) {
-        cout << "FAILED circle non-default constructor point test" << endl;
-    }
-    if (c1.getArea() != PI * 10 * 10) {
-        cout << "FAILED circle non-default constructor area test" << endl;
-    }
-    if (c1.getFill().red != 0 && c1.getFill().blue != 0 && c1.getFill().green != 0) {
-        cout << "FAILED Circle non-default constructor fill test" << endl;
-    }
-    if (c1.getPerimeter() != 2 * PI * 10) {
-        cout << "FAILED Circle non-default constructor perimeter test" << endl;
-    }
-
-    Circle c2(10, {1, 1});
-    if (c2.getCenter().x != 1 && c2.getCenter().y != 1) {
-        cout << "FAILED circle non-default constructor point test" << endl;
-    }
-    Circle c3(10, 2, 2);
-    if (c3.getCenter().x != 2 && c3.getCenter().y != 2) {
-        cout << "FAILED circle non-default constructor two-val point test" << endl;
-    }
-    Circle c4(3, 3);
-    if (c4.getCenter().x != 3 && c4.getCenter().y != 3) {
-        cout << "FAILED circle non-default constructor two-val point test" << endl;
-    }
-
-    /************* TESTING FOR ROCKET CLASS ************/
-
-    Rocket rock;
-    if (rock.getHullStat() != notDamaged) {
-        cout << "FAILED rocket default constructor hullstatus" << endl;
-    }
-    if (rock.getFuelTank() != 100) {
-        cout << "FAILED rocket default constructor FuelTank" << endl;
-    }
-    rock.setFuelTank(20);
-    if (rock.getFuelTank() != 20) {
-        cout << "FAILED rocket default constructor getter/setter FuelTank" << endl;
-    }
-    rock.setFuelTankToFull();
-    if (rock.getFuelTank() != 100) {
-        cout << "FAILED rocket default constructor getter/setter FuelTank" << endl;
-    }
-    rock.sethullStat(Damaged);
-    if (rock.getHullStat() != Damaged) {
-        cout << "FAILED rocket default constructor getter/setter HullStat" << endl;
-    }
-    rock.setRocketPos(0,0);
-    if (rock.getPosX() != 0 && rock.getPosX()!= 0){
-        cout << "FAILED rocket default constructor getter/setter point test" << endl;
-    }
-
-    /*********FUEL TANK TESTING **************/
-
-    fuelTank f;
-
-    if (f.getFuel()!= 5){
-        cout << "FAILED fuelTank default constructor init/getFuel test" << endl;
-    }
-    f.setFuel(400);
-    if (f.getFuel()!= 400){
-        cout << "FAILED fuelTank default constructor init/setFuel test" << endl;
-    }
-    f.useFuel();
-    if(f.getFuel()!= 399){
-        cout << "FAILED fuelTank usefuel" << endl;
-    }
-}
+//using namespace std;
+//
+//int main() {
+//    /********** TESTING FOR RECT CLASS ***********/
+//
+//    //default constructor testing
+//    Rect r;
+//    if (r.getCenter().x != 0 && r.getCenter().y != 0) {
+//        cout << "FAILED rectangle default constructor point test" << endl;
+//    }
+//    if (r.getArea() != 0) {
+//        cout << "FAILED Rectangle default constructor area test" << endl;
+//    }
+//    if (r.getFill().red != 0 && r.getFill().blue != 0 && r.getFill().green != 0) {
+//        cout << "FAILED Rectanble default constructor fill test" << endl;
+//    }
+//    if (r.getPerimeter() != 0) {
+//        cout << "FAILED Rectangle default constructor perimeter test" << endl;
+//    }
+//    //non-default testing 1
+//    Rect r1(0.0, 0.0);
+//    r1.setDimensions(2, 2);
+//    if (r1.getWidth() != 2 && r1.getHeight() != 2) {
+//        cout << "FAILED rect non-default constructor getwidth/setdim test" << endl;
+//    }
+//
+//    if (r1.getPerimeter() != 0) {
+//        cout << "FAILED shape non-default constructor perimeter test" << endl;
+//    }
+//
+//    // Getters and setters testing
+//    r.setPoint({1, 1});
+//    if (r.getCenter().x != 1 && r.getCenter().y != 1) {
+//        cout << "FAILED shape getter and setter point 1-val test" << endl;
+//    }
+//    r.setPoint(2, 2);
+//    if (r.getCenter().x != 2 && r.getCenter().y != 2) {
+//        cout << "FAILED shape getter and setter point 2-val test" << endl;
+//    }
+//    r.move(2, 2);
+//    if (r.getCenter().x != 4 && r.getCenter().y != 4) {
+//        cout << "FAILED shape getter and setter move test" << endl;
+//    }
+//    if (r.getPerimeter() != 8) {
+//        cout << "FAILED rect perimiter test" << endl;
+//    }
+//    if (r.getArea() != 4) {
+//        cout << "FAILED shape area test" << endl;
+//    }
+//
+//    /*************** TESTING FOR CIRCLE CLASS *************/
+//
+//    Circle c;
+//    if (c.getCenter().x != 0 && c.getCenter().y != 0) {
+//        cout << "FAILED circle default constructor point test" << endl;
+//    }
+//    if (c.getArea() != 0) {
+//        cout << "FAILED circle default constructor area test" << endl;
+//    }
+//    if (c.getFill().red != 0 && c.getFill().blue != 0 && c.getFill().green != 0) {
+//        cout << "FAILED Circle default constructor fill test" << endl;
+//    }
+//    if (c.getPerimeter() != 0) {
+//        cout << "FAILED Circle default constructor perimeter test" << endl;
+//    }
+//    //non-default
+//    Circle c1(10);
+//    if (c1.getCenter().x != 0 && c1.getCenter().y != 0) {
+//        cout << "FAILED circle non-default constructor point test" << endl;
+//    }
+//    if (c1.getArea() != PI * 10 * 10) {
+//        cout << "FAILED circle non-default constructor area test" << endl;
+//    }
+//    if (c1.getFill().red != 0 && c1.getFill().blue != 0 && c1.getFill().green != 0) {
+//        cout << "FAILED Circle non-default constructor fill test" << endl;
+//    }
+//    if (c1.getPerimeter() != 2 * PI * 10) {
+//        cout << "FAILED Circle non-default constructor perimeter test" << endl;
+//    }
+//
+//    Circle c2(10, {1, 1});
+//    if (c2.getCenter().x != 1 && c2.getCenter().y != 1) {
+//        cout << "FAILED circle non-default constructor point test" << endl;
+//    }
+//    Circle c3(10, 2, 2);
+//    if (c3.getCenter().x != 2 && c3.getCenter().y != 2) {
+//        cout << "FAILED circle non-default constructor two-val point test" << endl;
+//    }
+//    Circle c4(3, 3);
+//    if (c4.getCenter().x != 3 && c4.getCenter().y != 3) {
+//        cout << "FAILED circle non-default constructor two-val point test" << endl;
+//    }
+//
+//    /************* TESTING FOR ROCKET CLASS ************/
+//
+//    Rocket rock;
+//    if (rock.getHullStat() != notDamaged) {
+//        cout << "FAILED rocket default constructor hullstatus" << endl;
+//    }
+//    if (rock.getFuelTank() != 100) {
+//        cout << "FAILED rocket default constructor FuelTank" << endl;
+//    }
+//    rock.setFuelTank(20);
+//    if (rock.getFuelTank() != 20) {
+//        cout << "FAILED rocket default constructor getter/setter FuelTank" << endl;
+//    }
+//    rock.setFuelTankToFull();
+//    if (rock.getFuelTank() != 100) {
+//        cout << "FAILED rocket default constructor getter/setter FuelTank" << endl;
+//    }
+//    rock.sethullStat(Damaged);
+//    if (rock.getHullStat() != Damaged) {
+//        cout << "FAILED rocket default constructor getter/setter HullStat" << endl;
+//    }
+//    rock.setRocketPos(0,0);
+//    if (rock.getPosX() != 0 && rock.getPosX()!= 0){
+//        cout << "FAILED rocket default constructor getter/setter point test" << endl;
+//    }
+//
+//    /*********FUEL TANK TESTING **************/
+//
+//    fuelTank f;
+//
+//    if (f.getFuel()!= 5){
+//        cout << "FAILED fuelTank default constructor init/getFuel test" << endl;
+//    }
+//    f.setFuel(400);
+//    if (f.getFuel()!= 400){
+//        cout << "FAILED fuelTank default constructor init/setFuel test" << endl;
+//    }
+//    f.useFuel();
+//    if(f.getFuel()!= 399){
+//        cout << "FAILED fuelTank usefuel" << endl;
+//    }
+//}
